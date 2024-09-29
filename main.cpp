8cff3749aa3e5e3d93cce945f04d164a621c640c
NINA <ninatsao@ningningning.lan>
2024-09-29 16:13:24 -0700

    2nd commit

--- main.cpp
+++ main.cpp
@@ -3,16 +3,31 @@
 #include <iostream>
 using namespace std;
 
+// Movie class
 class Movie {
+// private member variables
 private:
     string screenWriter;
     int yearReleased;
     string title;
+
+// public setters & getters for each private member variable
 public:
     string getScreenWriter() const {return screenWriter;}
-    int getYearReleased() const {return yearReleased;}
-    string getTitle() const {return title;}
+    void setScreenWiter(string s) {screenWriter = s;}
 
+    int getYearReleased() const {return yearReleased;}
+    void setYearReleased(int y) {yearReleased = y;}
+
+    string getTitle() const {return title;}
+    void setTitle(string t) {title = t;}
+
+    // print() method which prints the object data
+    void print() {
+        cout << "\tMovie: " << title << endl;
+        cout << "\t\tYear released: " << yearReleased << endl;
+        cout << "\t\tScreenwriter: " << screenWriter << endl;
+    }
 };
 
 int main() {
